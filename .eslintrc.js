8e6ee676b6ca05aebcb6e545a2ba3c4b50cf42d9
Juseong Kim <khackskjs@gmail.com>
2021-08-13 00:28:58 +0900

    Change lint settings

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module',
+  },
+  extends: ['plugin:vue/recommended', 'eslint:recommended'],
+  plugins: ['prettier'],
+  rules: {
+    'no-debugger': process.env.PRE_COMMIT ? 'error' : 'off',
+    'no-console': process.env.PRE_COMMIT
+      ? ['error', { allow: ['warn', 'error'] }]
+      : 'off',
+    'no-extra-semi': 'error',
+    'no-empty-pattern': 'off',
+    'no-case-declarations': 'off',
+    'no-useless-escape': 'off',
+    camelcase: ['error', { properties: 'never', ignoreDestructuring: true }],
+    quotes: ['error', 'single'],
+    indent: ['error', 2, { SwitchCase: 1 }],
+    semi: ['error', 'never'],
+    'vue/no-v-html': ['off'],
+    'vue/multiline-html-element-content-newline': 'error',
+    'vue/singleline-html-element-content-newline': 'error',
+    'vue/no-spaces-around-equal-signs-in-attribute': 'error',
+    'vue/script-indent': ['off', null, { baseIndent: 0 }],
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: 1,
+        multiline: {
+          max: 1,
+          allowFirstLine: false,
+        },
+      },
+    ],
+  },
+}
