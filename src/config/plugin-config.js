54b9be365ba5e5208d682d0cad82df68a63ecedf
Juseong Kim <khackskjs@gmail.com>
2021-08-14 22:12:47 +0900

    Add vue material

---
+++ src/config/plugin-config.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+
+/*** for BootstrapVue ***/
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+
+/*** Vue Material ***/
+import { MdButton, MdContent, MdTabs, MdField } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(MdButton)
+Vue.use(MdContent)
+Vue.use(MdTabs)
+Vue.use(MdField)
