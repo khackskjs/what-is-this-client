5fdee52ba92d3cc19365235da60ed4a53d8acd32
Juseong Kim <khackskjs@gmail.com>
2021-08-19 00:55:59 +0900

    Add card group mock

--- src/services/api/_api.mock.js
+++ src/services/api/_api.mock.js
@@ -2,6 +2,8 @@ const mock = {
   cardGroupList: [
     createCardGroup('cg1-uid', 'c-group-1'),
     createCardGroup('cg2-uid', 'c-group-2'),
+    createCardGroup('cg3-uid', '3 일차'),
+    createCardGroup('cg4-uid', '4 일차'),
   ],
   cardList: [
     createCard('cg1-uid', 'c01-uid', 'ENGLISH', '한글'),
@@ -10,6 +12,12 @@ const mock = {
     createCard('cg2-uid', 'c11-uid', 'Watch', '시계'),
     createCard('cg2-uid', 'c12-uid', 'Game', '오락'),
     createCard('cg2-uid', 'c13-uid', 'Fan', '선풍기'),
+    createCard('cg3-uid', 'c21-uid', 'Browser', '브라우저'),
+    createCard('cg3-uid', 'c22-uid', 'Monitor', '모니터'),
+    createCard('cg3-uid', 'c23-uid', 'Mouse', '쥐'),
+    createCard('cg4-uid', 'c31-uid', 'Keyboard', '키보드'),
+    createCard('cg4-uid', 'c32-uid', 'ipad', '아이패드'),
+    createCard('cg4-uid', 'c33-uid', 'speaker', '스피커'),
   ],
 }
 function createCardGroup(guid, name) {
