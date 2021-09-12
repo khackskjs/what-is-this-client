bd71455298fbdd0bbb2033fc1d40bf5c88d62e2f
Juseong Kim <khackskjs@gmail.com>
2021-09-12 21:19:03 +0900

    Change router file name

---
+++ src/router/router.js
@@ -0,0 +1,80 @@
+// eslint-disable-next-line prettier/prettier
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import CardManagement from '../views/CardManagement.vue'
+import CardGroupListPage from '../views/CardGroupListPage.vue'
+import CardGroupForm from '../components/forms/CardGroupForms.vue'
+import authService from '@services/auth'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+    meta: {
+      allowNotAuthenticated: true
+    },
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+    meta: {
+      allowNotAuthenticated: true
+    },
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+  {
+    path: '/card-management',
+    component: CardManagement,
+    meta: { requiresAuth: true },
+    children: [
+      {
+        path: '/',
+        name: 'card-list',
+        component: CardGroupListPage,
+      },
+      {
+        path: '/edit',
+        component: CardGroupForm,
+      },
+    ]
+  },
+
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+})
+
+router.beforeEach((to, from, next) => {
+  console.log('to', to.meta)
+  if (to.matched.some(record => record.meta.allowNotAuthenticated)) {
+    console.log('pass /login')
+    next()
+  } else {
+    if (authService.isAuthenticated()) {
+      console.log('PASS auth')
+      next()
+    } else {
+      console.log('redirect to /login')
+      next({ name: 'Login' })
+    }
+  }
+})
+
+export default router
