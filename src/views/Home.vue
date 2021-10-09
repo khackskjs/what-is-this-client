fcb92570a35b98e1f540082a6bea91f63c751081
Juseong Kim <khackskjs@gmail.com>
2021-10-09 19:46:14 +0900

    Fix to fit height on Home path

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,36 @@
 <template>
-  <div>
-    <div class="md-layout min-vh-100">
-      <router-link
-        to="/review"
-        tag="md-button"
-        class="md-layout-item md-small-size-100 md-raised md-primary h-auto"
-      >
-        <md-icon class="md-size-3x">
-          school
-        </md-icon>
-      </router-link>
-      <router-link
-        to="/management"
-        tag="md-button"
-        class="md-layout-item md-small-size-100 md-raised md-accent h-auto menu-icon-size"
-      >
-        <md-icon class="md-size-3x">
-          post_add
-        </md-icon>
-      </router-link>
-    </div>
+  <div class="md-layout" :style="{ height: `${deviceInnerHeight}px` }">
+    <router-link
+      to="/review"
+      tag="md-button"
+      class="md-layout-item md-small-size-100 md-raised md-primary h-auto"
+    >
+      <md-icon class="md-size-3x">
+        school
+      </md-icon>
+    </router-link>
+    <router-link
+      to="/management"
+      tag="md-button"
+      class="md-layout-item md-small-size-100 md-raised md-accent h-auto menu-icon-size"
+    >
+      <md-icon class="md-size-3x">
+        post_add
+      </md-icon>
+    </router-link>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import { mapGetters } from 'vuex'
 
 export default {
   name: 'Home',
   components: {},
+  computed: {
+    ...mapGetters(['deviceInnerHeight']),
+  },
 }
 </script>
 
